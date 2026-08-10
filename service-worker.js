61bcc7ea9c64d7af39a169b5fb09b2d39c443a6a
Sunarso29 <sunarso29@gmail.com>
2026-08-11 04:48:45 +0700

    Refresh final class session permission runtime

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="paibp-smart-v102-global-gate-fast-r5";
+const CACHE_NAME="paibp-smart-v102-global-gate-fast-r6";
 const STATIC=[
   "./","./index.html","./fitur.html","./mapel-lain.html","./literasi-digital.html","./artikel-islam.html","./about-spensus.html","./contact.html","./akses-guru.html",
   "./logo-spensus.png","./gerbang.jpg","./assets/icons/icon-192.png",
