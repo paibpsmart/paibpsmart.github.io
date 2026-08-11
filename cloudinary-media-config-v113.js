3a8da960fe3e6f5c5015f053d73fbea6efbef5e0
Sunarso29 <sunarso29@gmail.com>
2026-08-12 04:35:40 +0700

    Tambah konfigurasi media Cloudinary gratis V113

---
+++ cloudinary-media-config-v113.js
@@ -0,0 +1,8 @@
+window.PAIBP_CLOUDINARY_MEDIA={
+  cloudName:"k2mss5sn",
+  uploadPreset:"",
+  folder:"paibp-smart/news",
+  maxVideos:5,
+  maxVideoBytes:100*1024*1024,
+  maxAudioBytes:25*1024*1024
+};
