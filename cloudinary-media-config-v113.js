edb99ec6a53ddafdb877c58d41fa7e0a4aebe72f
Sunarso29 <sunarso29@gmail.com>
2026-08-12 04:39:29 +0700

    Tetapkan preset Cloudinary gratis PAIBP

--- cloudinary-media-config-v113.js
+++ cloudinary-media-config-v113.js
@@ -1,7 +1,6 @@
 window.PAIBP_CLOUDINARY_MEDIA={
   cloudName:"k2mss5sn",
-  uploadPreset:"",
-  folder:"paibp-smart/news",
+  uploadPreset:"paibp_media_free",
   maxVideos:5,
   maxVideoBytes:100*1024*1024,
   maxAudioBytes:100*1024*1024
