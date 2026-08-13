eaa0c524304685ea2d3188318a706163500a4741
Sunarso29 <sunarso29@gmail.com>
2026-08-13 23:29:55 +0700

    V146 cache bust loader share publik

--- home-share-v102.js
+++ home-share-v102.js
@@ -1,9 +1,9 @@
 (()=>{
 "use strict";
-if(window.__SPENSUS_SHARE_V133__||document.querySelector('script[data-share-v133]'))return;
+if(window.__SPENSUS_SHARE_V146__||document.querySelector('script[data-share-v146]'))return;
 const s=document.createElement('script');
-s.dataset.shareV133='1';
-s.src=new URL('home-share-v133.js?v=133',document.baseURI).href;
+s.dataset.shareV146='1';
+s.src=new URL('home-share-v133.js?v=146',document.baseURI).href;
 s.async=false;
 document.head.appendChild(s);
 })();
